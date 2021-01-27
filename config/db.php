5183b3075ac80a28cd1aff3fe91cbf732cbe2626
Vidush Dixit <32012141+vidush-dixit@users.noreply.github.com>
2021-01-27 20:41:32 +0530

    Hosting remote DB

--- config/db.php
+++ config/db.php
@@ -1,9 +1,16 @@
 <?php
 
-    $serverName = "localhost";
-    $serverUser = "root";
-    $serverPass = "";
-    $dbName     = "cms";
+//  For Development Purpose
+//  $serverName = "localhost";
+//  $serverUser = "root";
+//  $serverPass = "";
+//  $dbName     = "cms";
+
+//  For Production Purpose - Remote Database(remotemysql.com)
+    $serverName = "remotemysql.com";
+    $serverUser = "RnVyo0oCAD";
+    $serverPass = "WyHPISLeaQ";
+    $dbName     = "RnVyo0oCAD";
 
     $conn       = mysqli_connect($serverName, $serverUser, $serverPass, $dbName) or die("Err! Connection Failed!!" + mysqli_connect_error());
 
